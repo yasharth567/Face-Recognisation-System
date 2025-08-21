1c2179b267eeb587301f0e7ba2edafdba4c72d98
Yasharth Singh <160147894+yasharth567@users.noreply.github.com>
2025-08-21 12:56:15 +0530

    Update and rename next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
@@ -10,4 +9,5 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
